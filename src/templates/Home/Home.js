ac716653d01afdfe3ff503a0b0363a36071acb59
s8_vaitku_e <s8_vaitku_e@venta.lv>
2020-11-11 09:58:29 +0000

    Add new file

---
+++ src/templates/Home/Home.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+  
+class Home extends Component{
+	render(){ 
+		return ( 
+			<div> 
+				<h3>Par</h3>
+				<p>Es izveidoju React tipa projektu, kura izveidoju divus logus:</p>
+				<ul>
+					<li>
+						<p>Sakums</p>
+					</li>
+					<li>
+						<p>Spele</p>
+					</li>
+				</ul>
+				<p>Lai izveidotu so projektu, es izveidoju js un cscc failus.</p>
+				
+			</div> 
+		) 
+	} 
+}
+export default Home
