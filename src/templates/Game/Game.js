946b0ed07ac2486bc96d712dee641a162a557995
s8_vaitku_e <s8_vaitku_e@venta.lv>
2020-11-11 09:55:32 +0000

    Update Game.js

--- src/templates/Game/Game.js
+++ src/templates/Game/Game.js
@@ -1,17 +1,42 @@
-import GuessTheNumber from 'components/guessTheNumber';
-
-import './Game.scss';
-
-const Game = () => {
-  return (
-    <div className="Game">
-      <section>
-        
-		<GuessTheNumber />
-        
-      </section>
-    </div>
-  );
-};
-
-export default Game;
+import React, { Component } from 'react'
+import Result from './Result'
+  
+class Game extends Component{ 
+  
+	static defaultProps = {secret : Math.floor(Math.random() * 100) + 1} 
+	  
+	constructor(props){
+		super(props)
+		this.state = { term : '' } 
+		this.handleChange = this.handleChange.bind(this)
+	} 
+	  
+	handleChange(event){ 
+		this.setState({ 
+		[event.target.name] : event.target.value 
+		}) 
+	} 
+	  
+	render(){ 
+		return ( 
+			<div> 
+				<div> 
+					<p>Vel neatjaunina minamo skaitli kad atminets (F5 lai atj.).</p>
+					<label htmlFor='term'> 
+						<p>Mini skaitli no 1 lidz 100</p>
+					</label> 
+				</div> 
+				<input 
+					id='term' 
+					type='text' 
+					name='term' 
+					value={this.state.term}  
+					onChange={this.handleChange}  
+				/> 
+		
+				<Result term={this.state.term} secretNum={this.props.secret} /> 
+			</div> 
+		) 
+	} 
+} 
+export default Game
